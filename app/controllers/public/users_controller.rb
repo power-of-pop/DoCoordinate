00065c4ee0f3ca4d7fe7376581faa88b2390e16e
EC2 Default User <ec2-user@ip-172-31-39-216.ap-northeast-1.compute.internal>
2025-02-07 08:08:34 +0000

    update

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,16 +1,29 @@
 class Public::UsersController < ApplicationController
 
   def show
-    @user = current_user
+    @user = User.find(params[:id])
+    @posts = @user.posts
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "You have updated user successfully."
+      redirect_to user_path(@user.id)
+    else
+      render :edit
+    end
   end
 
   def destroy
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :profile_image, :introduction)
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,13 @@ class User < ApplicationRecord
 
   has_many :posts, dependent: :destroy
 
-  def get_profile_image
-    (profile_image.attached?) ? profile_image : 'no_image_471.jpg'
+
+  def get_profile_image(width,height)
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image_471.jpg')
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    profile_image.variant(resize_to_limit: [width, height]).processed
   end
+  
 end
