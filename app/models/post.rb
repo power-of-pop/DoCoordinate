4821153bf32b5d947f6bcb638482ae01fc0b65e4
EC2 Default User <ec2-user@ip-172-31-39-216.ap-northeast-1.compute.internal>
2025-02-10 12:37:52 +0000

    edit layout

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,9 +2,9 @@ class Post < ApplicationRecord
   has_one_attached :post_image
   belongs_to :user
 
+  validates :post_image, presence: true
   validates :title, presence: true
   validates :body, presence: true
-  validates :post_image, presence: true
 
   def get_post_image
     (post_image.attached?) ? post_image : 'no_image_471.jpg'
