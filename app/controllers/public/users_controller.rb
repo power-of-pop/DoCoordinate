48de17aad027eafba3bffc14e412165b85ff2b97
EC2 Default User <ec2-user@ip-172-31-39-216.ap-northeast-1.compute.internal>
2025-02-22 08:02:23 +0000

    ユーザー詳細表示

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,20 +1,15 @@
 class Public::UsersController < ApplicationController
   before_action :authenticate_user!, except: [:top, :about]
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-  before_action :correct_user, only: [:show, :edit, :update, :destroy]
   before_action :ensure_guest_user, only: [:edit]
 
   def show
-    @user = User.find(params[:id])
     @posts = @user.posts
   end
 
-  def edit
-    @user = User.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "ユーザー情報が更新されました。"
       redirect_to user_path(@user.id)
@@ -25,7 +20,6 @@ class Public::UsersController < ApplicationController
 
   # 退会機能
   def destroy
-    @user = User.find(params[:id]) 
     @user.destroy
     flash[:notice] = '退会しました。'
     redirect_to new_user_registration_path
@@ -37,14 +31,6 @@ class Public::UsersController < ApplicationController
 
   private
 
-  def correct_user
-    @user = User.find_by(id: params[:id])
-    if @user.nil?
-      redirect_to user_path(current_user)
-    else
-      redirect_to user_path(current_user) unless current_user == @user
-    end
-  end
 
   def ensure_guest_user
     @user = User.find(params[:id])
@@ -54,7 +40,6 @@ class Public::UsersController < ApplicationController
   end
 
   def set_user
-     @user = User.find_by(:id => params[:id])
+     @user = User.find(params[:id])
   end
-
 end
