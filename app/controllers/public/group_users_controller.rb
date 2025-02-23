02add7f79836f97b013a5912134df5e38c1102b8
EC2 Default User <ec2-user@ip-172-31-39-216.ap-northeast-1.compute.internal>
2025-02-23 03:29:23 +0000

    実装フェーズ3完了

--- app/controllers/public/group_users_controller.rb
+++ app/controllers/public/group_users_controller.rb
@@ -1,6 +1,7 @@
 class Public::GroupUsersController < ApplicationController
   before_action :authenticate_user!
 
+  # 参加申請
   def create
     @group = Group.find(params[:group_id])
     @permit = Permit.find(params[:permit_id])
@@ -9,6 +10,7 @@ class Public::GroupUsersController < ApplicationController
     redirect_to request.referer
   end
 
+  # 申請取消
   def destroy
     group_user = current_user.group_users.find_by(group_id: params[:group_id])
     group_user.destroy
@@ -20,4 +22,5 @@ class Public::GroupUsersController < ApplicationController
     @permit.destroy
     redirect_to request.referer
   end
+
 end
--- app/controllers/public/permits_controller.rb
+++ app/controllers/public/permits_controller.rb
@@ -1,5 +1,6 @@
 class Public::PermitsController < ApplicationController
   before_action :authenticate_user!
+  before_action :ensure_guest_user, only: [:create, :destroy]
 
   def create
     @group = Group.find(params[:group_id])
@@ -12,5 +13,14 @@ class Public::PermitsController < ApplicationController
     permit = current_user.permits.find_by(group_id: params[:group_id])
     permit.destroy
     redirect_to request.referer, alert: "コミュニティへの参加申請を取り消ししました。"
+  end
+
+  private
+
+  def ensure_guest_user
+    @user = current_user
+    if @user.email == "guest@example.com"
+      redirect_to request.referer , notice: "ゲストユーザーの操作は制限されています。"
+    end
   end
 end
