032bc0e75c3f20520629415d70bef304a6aa509f
EC2 Default User <ec2-user@ip-172-31-39-216.ap-northeast-1.compute.internal>
2025-03-09 12:46:14 +0000

    ゲストユーザー画像

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,6 +21,12 @@ class User < ApplicationRecord
       file_path = Rails.root.join('app/assets/images/no_image_471.png')
       profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
+
+    if self.guest_user?
+      file_path = Rails.root.join('app/assets/images/guestuser.png')
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+
     profile_image.variant(resize_to_limit: [width, height]).processed
   end
 
